878b87e0cdb36824e2093f4f234bd39774d828ea
Matheus Souza <ms.conrado25@hotmail.com>
2021-06-23 23:43:58 -0300

    exercicio 3 - retorne o maior dos tres

--- FundamentosWeb/bloco_4_JavaScript/dia_1_Introdução/javascrip.js
+++ FundamentosWeb/bloco_4_JavaScript/dia_1_Introdução/javascrip.js
@@ -9,11 +9,24 @@
 // console.log(a % b);
 
 // Exercício 2
-const a = 20;
-const b = 15;
+// const a = 20;
+// const b = 15;
 
-if(a > b){
-  console.log('a maior')
-} else {
-  console.log('b maior')
+// if(a > b){
+//   console.log('a maior');
+// } else {
+//   console.log('b maior');
+// }
+
+// Exercício 3
+const a = 40;
+const b = 50;
+const c = 30;
+
+if(a > b && a > c){
+  console.log('a é maior');
+} else if (b > a && b > c){
+  console.log('b é maior');
+} else{
+  console.log('c é maior');
 }
