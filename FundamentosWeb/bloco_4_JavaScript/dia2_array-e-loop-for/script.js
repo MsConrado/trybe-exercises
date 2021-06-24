325c917976fd6f0985334ce4308f4e0f740fa16d
Matheus Souza <ms.conrado25@hotmail.com>
2021-06-24 19:12:40 -0300

    execercicos 3 a 6

--- FundamentosWeb/bloco_4_JavaScript/dia2_array-e-loop-for/script.js
+++ FundamentosWeb/bloco_4_JavaScript/dia2_array-e-loop-for/script.js
@@ -7,10 +7,57 @@
 
 // 2
 
-let soma = 0;
+// let soma = 0;
 
-for(let index = 0; index < numbers.length; index += 1){
-  soma += numbers[index];
-  
-}
-console.log(soma);
+// for(let index = 0; index < numbers.length; index += 1){
+//   soma += numbers[index];
+
+// }
+// console.log(soma);
+
+// 3
+
+// let soma = 0
+
+// for(let index = 0; index < numbers.length; index += 1){
+//   soma += numbers[index];
+// }
+// let aritimetica = soma / numbers.length;
+// console.log(aritimetica);
+
+// 4
+
+// let soma = 0
+
+// for(let index = 0; index < numbers.length; index += 1){
+//   soma += numbers[index];
+// }
+// let aritimetica = soma / numbers.length;
+
+// if(aritimetica > 20){
+//   console.log('Maior que 20');
+// } else {
+//   console.log('Valor menor que 20');
+// }
+
+// 5
+
+// let maior = 0
+
+// for(let num of numbers){
+//   if(maior < num){
+//     maior = num;
+//   }
+// }
+
+// console.log(maior)
+
+// let menor = numbers[0]
+
+// for(let num of numbers){
+//   if(menor > num){
+//     menor = num;
+//   }
+// }
+
+// console.log(menor)
