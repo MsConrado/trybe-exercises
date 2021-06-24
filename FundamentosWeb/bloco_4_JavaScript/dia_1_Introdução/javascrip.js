67977008fda70f5bd8b61f503744a00a1535e030
Matheus Souza <ms.conrado25@hotmail.com>
2021-06-23 23:49:34 -0300

    exercicio 4 - positivo e negativo

--- FundamentosWeb/bloco_4_JavaScript/dia_1_Introdução/javascrip.js
+++ FundamentosWeb/bloco_4_JavaScript/dia_1_Introdução/javascrip.js
@@ -19,14 +19,25 @@
 // }
 
 // Exercício 3
-const a = 40;
-const b = 50;
-const c = 30;
+// const a = 40;
+// const b = 50;
+// const c = 30;
 
-if(a > b && a > c){
-  console.log('a é maior');
-} else if (b > a && b > c){
-  console.log('b é maior');
-} else{
-  console.log('c é maior');
+// if(a > b && a > c){
+//   console.log('a é maior');
+// } else if (b > a && b > c){
+//   console.log('b é maior');
+// } else{
+//   console.log('c é maior');
+// }
+
+// Exercício 4
+let valor = 10;
+
+if(valor > 0){
+  console.log('positivo');
+} else if (valor < 0){
+  console.log('negativo');
+} else {
+  console.log('zero')
 }
