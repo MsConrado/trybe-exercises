4dea00aad779e791e891521763bc2a702754039b
Matheus Souza <ms.conrado25@hotmail.com>
2021-06-23 23:39:41 -0300

    exercicio 2 - retorne o maior valor

--- FundamentosWeb/bloco_4_JavaScript/dia_1_Introdução/javascrip.js
+++ FundamentosWeb/bloco_4_JavaScript/dia_1_Introdução/javascrip.js
@@ -1,9 +1,19 @@
-// Exercício 1
-let a = 5;
-let b = 20;
+// // Exercício 1
+// let a = 5;
+// let b = 20;
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// Exercício 2
+const a = 20;
+const b = 15;
+
+if(a > b){
+  console.log('a maior')
+} else {
+  console.log('b maior')
+}
