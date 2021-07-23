f2093bcb522c58b9e6c7c3c421a8bd5e420242ae
Matheus Souza <ms.conrado25@hotmail.com>
2021-07-23 09:23:44 -0300

    ...

--- FundamentosWeb/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/scripts.js
+++ FundamentosWeb/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/scripts.js
@@ -16,18 +16,18 @@
 
 // testingScope(true);
 
-const testingScope = escopo  => {
-  if (escopo === true) {
-    let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-    console.log(ifScope);
-  } else {
-    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
-}
+// const testingScope = escopo  => {
+//   if (escopo === true) {
+//     let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+// }
 
-testingScope(true);
+// testingScope(true);
 
 // 2 - Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
 // Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
@@ -37,37 +37,41 @@ testingScope(true);
 
 // Seu código aqui.
 
-// console.log(oddsAndEvens); // será necessário alterar essa linha 
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+// console.log(oddsAndEvens); // será necessário alterar essa linha
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-const ordemCrescente = () => {
-  oddsAndEvens[0] = 2;
-  oddsAndEvens[1] = 3;
-  oddsAndEvens[2] = 4;
-  oddsAndEvens[3] = 7;
-  oddsAndEvens[4] = 10;
-  oddsAndEvens[5] = 13;
+// const ordemCrescente = () => {
+//   oddsAndEvens[0] = 2;
+//   oddsAndEvens[1] = 3;
+//   oddsAndEvens[2] = 4;
+//   oddsAndEvens[3] = 7;
+//   oddsAndEvens[4] = 10;
+//   oddsAndEvens[5] = 13;
 
-  return oddsAndEvens;
-}
-const funcao = ordemCrescente()
+//   return oddsAndEvens;
+// }
+// const funcao = ordemCrescente()
 
-console.log(`Os números ${ordemCrescente} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
+// console.log(`Os números ${ordemCrescente} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha
 
-// PARTE II 
+// PARTE II
 // Abaixo, você verá algumas especificações de algoritmos para desenvolver. É fundamental que você utilize o que aprendeu sobre let , const , arrow functions , template literals e ternary operator .
 // Crie uma função que receba um número e retorne seu fatorial.
 // Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
 // Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator .
 
-const fatorial = num => {
-    let result = 1;
+// const fatorial = num => {
+// let result = 1;
 
-    for (let index = 1; index <= number; index += 1) {
-        result *= index;
-    }
+// for (let index = 1; index <= num; index += 1) {
+// result *= index;
+// }
 
-    return result;
-}
+// return result;
+// }
 
-console.log(factorial(3));
+// console.log(fatorial(3));
+
+//  2 - Crie uma função que receba uma frase e retorne qual a maior pala
+
+
